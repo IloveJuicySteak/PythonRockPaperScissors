33b7bde3ac9b6e6ebcf8163371e251a5b13dc58b
IloveJuicySteak <gurung.sampurna1@gmail.com>
2025-05-05 13:35:55 +0100

    Create RPS.py
    Rock PAper Scissors game

---
+++ RPS.py
@@ -0,0 +1,31 @@
+import random 
+
+Options = ("Rock", "Paper", "Scissors")
+running = True 
+while running:
+
+  Player = none 
+
+  computer = random.choice(Options)
+  
+  while Player not in opitons: 
+  Player = inout("Enter a choice (Rock, Paper, Scissors): "
+  
+  print(f"Player: {Player}")
+  print(f"Computer: {computer}")
+  
+  if Player == computer:
+                 print ("It's a tie!")
+  elif Player == "Rock" and computer == "Scissors":
+                 print ("You win!")
+  elif Player == "Paper" and computer == "Rock":
+                 print ("You win!")
+  elif Player == "Scissors" and computer == "Paper":
+                 print ("You win!")
+  else:
+  print ("You Lose!")
+
+  if not input("Play again> (y/n): ").lower == "y":
+    running = False
+    
+print ("Thanks for playing!")
